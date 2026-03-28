2310094606652e799a078f68b49c24b1e59dfd4b
samisepp <samisepp@proton.me>
2026-03-28 07:29:43 +0200

    Delete ostolaskut/kaynnista_palvelin.bat

--- ostolaskut/kaynnista_palvelin.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Kaynnistetaan paikallinen palvelin...
-echo Avaa selaimeen: http://localhost:8000/salon_ostolaskut_kartta.html
-echo Sulje painamalla Ctrl+C
-cd /d "%~dp0"
-python -m http.server 8000
-pause
